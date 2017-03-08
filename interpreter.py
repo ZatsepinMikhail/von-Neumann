dd0fdf8ced6f10eac4f92cb405da8a019379e163
ZatsepinMikhail <salamence1996@gmail.com>
2017-03-08 16:05:54 +0300

    fibonacci using recursion. it works!

--- interpreter.py
+++ interpreter.py
@@ -12,6 +12,7 @@ commands_by_number = {}
 def MV(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
     if ptr_flag == 1:
         second_param = struct.unpack(pack_format, file_in_map[second_param: second_param + block_size])[0]
+    print(first_param, second_param)
     file_in_map[first_param: first_param + block_size] = file_in_map[second_param: second_param + block_size]
     file_in_map[0: block_size] = struct.pack(pack_format, instruction_ptr + block_size)
     return True
@@ -20,6 +21,7 @@ def MV(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
 def ADD(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
     first_value = struct.unpack(pack_format, file_in_map[first_param: first_param + block_size])[0]
     second_value = struct.unpack(pack_format, file_in_map[second_param: second_param + block_size])[0]
+    print(first_param, '(' + str(first_value) + ')', second_param, '(' + str(second_value) + ')')
     file_in_map[first_param: first_param + block_size] = struct.pack(pack_format, first_value + second_value)
     file_in_map[0: block_size] = struct.pack(pack_format, instruction_ptr + block_size)
     return True
@@ -27,7 +29,16 @@ def ADD(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
 
 def INC(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
     first_value = struct.unpack(pack_format, file_in_map[first_param: first_param + block_size])[0]
+    print(first_param, '(' + str(first_value) + ')')
     file_in_map[first_param: first_param + block_size] = struct.pack(pack_format, first_value + 1)
+    file_in_map[0: block_size] = struct.pack(pack_format, instruction_ptr + block_size)
+    return True
+
+
+def DEC(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
+    first_value = struct.unpack(pack_format, file_in_map[first_param: first_param + block_size])[0]
+    print(first_param, '(' + str(first_value) + ')')
+    file_in_map[first_param: first_param + block_size] = struct.pack(pack_format, first_value - 1)
     file_in_map[0: block_size] = struct.pack(pack_format, instruction_ptr + block_size)
     return True
 
@@ -51,12 +62,14 @@ def STOP(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
 def JUMP(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
     if ptr_flag == 1:
         first_param = struct.unpack(pack_format, file_in_map[first_param: first_param + block_size])[0]
+    print(first_param)
     file_in_map[0: block_size] = struct.pack(pack_format, first_param)
     return True
 
 
 def CJUMP(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
     bool_result = struct.unpack(pack_format, file_in_map[block_size: block_size + block_size])[0]
+    print(bool_result, first_param)
     if bool_result:
         file_in_map[0: block_size] = struct.pack(pack_format, first_param)
     else:
@@ -68,6 +81,7 @@ def CJUMP(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
 def CMPLE(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
     first_value = struct.unpack(pack_format, file_in_map[first_param: first_param + block_size])[0]
     second_value = struct.unpack(pack_format, file_in_map[second_param: second_param + block_size])[0]
+    print(first_value, second_value)
     if first_value <= second_value:
         file_in_map[block_size: block_size + block_size] = struct.pack(pack_format, 1)
     else:
@@ -78,17 +92,21 @@ def CMPLE(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
 
 def POP(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
     top_stack_ptr = struct.unpack(pack_format, file_in_map[2 * block_size: 3 * block_size])[0]
+    print(top_stack_ptr - block_size)
     file_in_map[2 * block_size: 3 * block_size] = struct.pack(pack_format, top_stack_ptr - block_size)
     file_in_map[0: block_size] = struct.pack(pack_format, instruction_ptr + block_size)
+    return True
 
 
 def PUSH(file_in_map, instruction_ptr, first_param, ptr_flag, second_param):
     top_stack_ptr = struct.unpack(pack_format, file_in_map[2 * block_size: 3 * block_size])[0]
     if ptr_flag == 1:
         first_param = struct.unpack(pack_format, file_in_map[first_param: first_param + block_size])[0]
+    print(first_param, top_stack_ptr + block_size)
     file_in_map[top_stack_ptr + block_size: top_stack_ptr + 2 * block_size] = struct.pack(pack_format, first_param)
     file_in_map[2 * block_size: 3 * block_size] = struct.pack(pack_format, top_stack_ptr + block_size)
     file_in_map[0: block_size] = struct.pack(pack_format, instruction_ptr + block_size)
+    return True
 
 
 def interprete(file_in_map):
@@ -97,6 +115,7 @@ def interprete(file_in_map):
     while not_stopped:
         instruction_ptr = struct.unpack(pack_format, file_in_map[0: block_size])[0]
         function_name = commands_by_number[file_in_map[instruction_ptr]][0]
+        print(instruction_ptr, function_name)
         not_stopped = globals()[function_name](file_in_map, instruction_ptr, file_in_map[instruction_ptr + 1], file_in_map[instruction_ptr + 2], file_in_map[instruction_ptr + 3])
 
 
